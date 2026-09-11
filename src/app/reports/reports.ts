1c6b0af14fcc85106b4ce6fd6e0bc19ce2763a79
PIYUSH SINGH <piyush.singh8744@gmail.com>
2026-09-11 22:55:37 +0530

    regular fix

--- src/app/reports/reports.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-reports',
-  styleUrl: './reports.css',
-  templateUrl: './reports.html',
-})
-export class Reports {}
